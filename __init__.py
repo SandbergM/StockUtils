54c1eeae23912870c040466faf0a2c3b79dc05fb
SandbergM <marcus.a.sandberg@outlook.com>
2023-06-16 22:59:01 +0200

    no msg

---
+++ __init__.py
@@ -0,0 +1,5 @@
+from nerd_stuff. EMA     import generate_ema
+from nerd_stuff. MACD    import generate_macd
+from nerd_stuff. RSI     import generate_rsi
+from nerd_stuff. SMA     import generate_sma
+from nerd_stuff. WMA     import generate_wma
---
+++ setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup, find_packages
+
+VERSION = '0.0.1' 
+DESCRIPTION = 'StockUtils'
+LONG_DESCRIPTION = 'StockUtils'
+
+setup(
+        name="StockUtils", 
+        version=VERSION,
+        author="John Doe",
+        author_email="John@Doe.com>",
+        description=DESCRIPTION,
+        long_description=LONG_DESCRIPTION,
+        packages=find_packages(),
+        install_requires=[],
+        keywords=['python'],
+        classifiers= []
+)
