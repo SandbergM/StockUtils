492da27d19f151ff4ce0c967706ef71d46137d94
SandbergM <marcus.a.sandberg@outlook.com>
2023-06-17 16:27:16 +0200

    msg

--- StockUtils/__init__.py
+++ StockUtils/__init__.py
@@ -1,5 +1,11 @@
 from . EMA     import generate_ema
+from . EMA     import generate_ema_signals
+
 from . MACD    import generate_macd
+from . MACD    import get_macd_signals
+
 from . RSI     import generate_rsi
+from . RSI     import get_rsi_signals
+
 from . SMA     import generate_sma
 from . WMA     import generate_wma
